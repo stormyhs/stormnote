23e1dd4f5d728467c62b4829d1bf0ff40e7be276
stormy <stormy.hs@protonmail.com>
2022-04-27 04:59:44 +0200

    idk i forgor

--- parser.js
+++ parser.js
@@ -3,17 +3,22 @@ function parse_content(content){
     parsed_content = "";
     content.forEach(line => {
         console.log(line);
-        if(line.substring(0, 4) == "### "){
-            parsed_content += `<h3 class="text-2xl">` + line.slice(4) + `</h3>`;
-        }
-        else if(line.substring(0, 3) == "## "){
-            parsed_content += `<h2 class="text-3xl">` + line.slice(3) + `</h2>`;
-        }
-        else if(line.substring(0, 2) == "# "){
-            parsed_content += `<h1 class="text-4xl">` + line.slice(2) + `</h1>`;
+        if(line == ""){
+            parsed_content += `<br>`;
         }
         else{
-            parsed_content += `<p class="text-lg">${line}</p>`;
+            if(line.substring(0, 4) == "### "){
+                parsed_content += `<h3 class="text-2xl">` + line.slice(4) + `</h3>`;
+            }
+            else if(line.substring(0, 3) == "## "){
+                parsed_content += `<h2 class="text-4xl">` + line.slice(3) + `</h2>`;
+            }
+            else if(line.substring(0, 2) == "# "){
+                parsed_content += `<h1 class="text-6xl">` + line.slice(2) + `</h1>`;
+            }
+            else{
+                parsed_content += `<p class="text-lg">${line}</p>`;
+            }
         }
     });
 
